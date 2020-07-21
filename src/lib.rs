57256019806a2b50c8b5a1b75a7c4f705a54f417
Phodal Huang <h@phodal.com>
2020-07-21 23:41:05 +0800

    feat: update deps

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub use class_parser::parse_class as parse_class;
+pub use class_parser::{ClassSignature, FieldSignature, MethodSignature};
+
+pub use classfile::attributes::Type as AttributeType;
+pub use classfile::constant_pool::Type as ConstantPoolType;
+
+pub use classfile::FieldInfo;
+pub use classfile::MethodInfo;
+pub use classfile::OpCode;
+pub use classfile::SignatureType;
+pub use classfile::types::{BytesRef, ConstantPool};
+pub use classfile::Version;
